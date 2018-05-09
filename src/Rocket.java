35e0c13c6a81153508c8ea49384be9c6fdcc31ff
agatin302 <agatin302@CHS-723-19.chs.win.fuhsd.org>
2018-05-09 09:34:59 -0700

    updated rocket class

--- src/Rocket.java
+++ src/Rocket.java
@@ -1,6 +1,11 @@
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 
 public class Rocket extends Actor{
+	
+	private double xSpeed;
+	private double ySpeed;
+	
 
 	public Rocket() {
 		setImage(new Image("images/paddle.png"));
@@ -8,7 +13,13 @@ public class Rocket extends Actor{
 	
 	@Override
 	public void act(long now) {
-		
+		if(getWorld().isKeyDown(KeyCode.LEFT)) setRotate(getRotate()+3.6);
+		if(getWorld().isKeyDown(KeyCode.RIGHT)) setRotate(getRotate() + 3.6);
+		if(getWorld().isKeyDown(KeyCode.SPACE)) fireBullet(getRotate());
+	}
+	
+	public void fireBullet(double angle) {
+		//getWorld().add();
 	}
 
 }
