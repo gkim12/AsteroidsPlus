f7e89f86ccd625b9742db2559b120170b72d8108
gkim198 <gkim198@CHS-723-20.chs.win.fuhsd.org>
2018-05-09 09:27:11 -0700

    Set Rocket image to Paddle (temporary), Added Asteroid Class

---
+++ src/Asteroid.java
@@ -0,0 +1,14 @@
+
+public class Asteroid extends Actor{
+
+	public Asteroid() {
+		
+	}
+
+	@Override
+	public void act(long now) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/Rocket.java
+++ src/Rocket.java
@@ -1,6 +1,11 @@
+import javafx.scene.image.Image;
 
 public class Rocket extends Actor{
 
+	public Rocket() {
+		setImage(new Image("images/paddle.png"));
+	}
+	
 	@Override
 	public void act(long now) {
 		
